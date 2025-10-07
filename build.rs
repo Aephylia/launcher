9ac2e0de057edcb4fea4494ddab1d2d07999e8dc
whosneksio <MARPLIC10@edu.gdansk.pl>
2025-10-07 21:44:06 +0200

    Thank you ChatGPT for this magnificent comment on build help :)

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 extern crate embed_resource;
 
 fn main() {
-    // Replace "app-manifest.rc" with the .rc filename you add below if you choose a different name
     embed_resource::compile("app-manifest.rc", embed_resource::NONE)
         .manifest_optional()
         .unwrap();
